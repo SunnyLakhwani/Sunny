73bdec3d32562702a3c4619406d8cddea3d2d1f8
SunnyLakhwani <sunnylakhwani1234@gmail.com>
2025-01-16 09:24:51 -0800

    Add files via upload

---
+++ CheckPalindrome.java
@@ -0,0 +1,15 @@
+class CheckPalindrome{
+    public static void main(String[] args) {
+        String x= "abcdrba";
+        int i=0, j=x.length() -1;
+        while(i < j){
+            if(x.charAt(i) != x.charAt(j)){
+                System.out.println("Not Palindrome");
+                System.exit(0);
+            }
+            i++;
+            j--;
+        }
+           System.out.println("Palindrome");
+    }
+}
